9d0dac429dd06195c948c109263cba03facf0bc6
BadangGit <156088018+BadangGit@users.noreply.github.com>
2025-01-30 12:27:43 +0900

    add file input and preview function

--- design-preloader/src/components/files/FileInput.tsx
+++ design-preloader/src/components/files/FileInput.tsx
@@ -1,3 +1,31 @@
+import React, { useState } from "react";
+
 export default function Filedrop() {
-    return <></>;
+    type InputFile = { tsx_file: File; preview_text: string };
+    const [file, setFile] = useState<InputFile | null>(null);
+
+    const previewFile = (e: any) => {
+        const inputFile: File = e.currentTarget.files[0];
+
+        if (inputFile) {
+            const reader = new FileReader();
+
+            reader.onload = (e: any) => {
+                setFile({
+                    tsx_file: inputFile,
+                    preview_text: e.target.result,
+                });
+            };
+
+            reader.readAsText(inputFile);
+        }
+    };
+
+    return (
+        <>
+            <input type="file" onChange={previewFile} accept=".tsx" />
+
+            <div> {file?.preview_text} </div>
+        </>
+    );
 }
