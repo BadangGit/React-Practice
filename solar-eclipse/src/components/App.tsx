614cc0a1068107afc87f42b01127837dd8fcc95d
BadangGit <156088018+BadangGit@users.noreply.github.com>
2025-04-05 18:04:35 +0900

    split SolarEclipse to new component

--- solar-eclipse/src/components/App.tsx
+++ solar-eclipse/src/components/App.tsx
@@ -1,36 +1,10 @@
 import React from "react";
 import "../styles/App.css";
 
-import Sun from "./orb/Sun";
-import Moon from "./orb/Moon";
-
-import createRandomColor from "../utils/colors";
+// import SolarEclipse from "./SolarEclipse";
 
 function App() {
-    const noonPostion = {
-        top: "100px",
-        left: "50%",
-    };
-
-    const orbShape = {
-        width: "40px",
-        height: "40px",
-        borderRadius: "50%",
-    };
-
-    const randomColor = createRandomColor();
-
-    return (
-        <div>
-            <Sun pos={noonPostion} shape={orbShape} shineColor={randomColor} />
-            <Moon pos={noonPostion} shape={orbShape} />
-
-            <div className="footer">
-                <h1>Solar Eclipse</h1>
-                <h3>drag the moon</h3>
-            </div>
-        </div>
-    );
+    return <div>{/* <SolarEclipse /> */}</div>;
 }
 
 export default App;
---
+++ solar-eclipse/src/components/SolarEclipse.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import Sun from "./orb/Sun";
+import Moon from "./orb/Moon";
+
+import createRandomColor from "../utils/colors";
+
+import styles from "../styles/SolarEclipse.module.css";
+
+const SolarEclipse: React.FC = () => {
+    const noonPostion = {
+        top: "100px",
+        left: "50%",
+    };
+
+    const orbShape = {
+        width: "40px",
+        height: "40px",
+        borderRadius: "50%",
+    };
+
+    const randomColor = createRandomColor();
+
+    return (
+        <div className={styles.app}>
+            <Sun pos={noonPostion} shape={orbShape} shineColor={randomColor} />
+            <Moon pos={noonPostion} shape={orbShape} />
+
+            <div className="footer">
+                <h1>Solar Eclipse</h1>
+                <h3>drag the moon</h3>
+            </div>
+        </div>
+    );
+};
+
+export default SolarEclipse;
