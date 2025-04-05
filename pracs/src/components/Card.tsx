0d531d9d7e6d3515a7b898fa806235e5d59bc34c
BadangGit <156088018+BadangGit@users.noreply.github.com>
2025-04-05 18:11:20 +0900

    change project name

---
+++ pracs/src/components/Card.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "../styles/Card.css";
+
+const Card: React.FC = () => {
+    return (
+        <div className="card">
+            <div className="card-content"></div>
+        </div>
+    );
+};
